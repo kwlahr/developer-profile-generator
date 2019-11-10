c006a00316b4f8d27cab08da1204036873fe513d
kwlahr <kwlahr@gmail.com>
2019-11-10 16:13:11 -0700

    added index.html generator in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const electronHTMLTo = require("electron-html-to");
 function promptUser() {
     return inquirer.prompt([{
             type: "input",
-            name: "Favorite Color",
+            name: "color",
             text: "What is your favorite color?"
         },
         {
@@ -17,15 +17,65 @@ function promptUser() {
             name: "username",
             text: "Enter your GitHub username here"
         }
-    ]).then(function (data) {
-        console.log(data);
-        axios.get(`https://api.github.com/users/${data.username}`)
-            .then(function (user) {
-                console.log(user.data);
-            })
-    })
+    ])
 }
 
 
 
-promptUser();
+promptUser()
+    .then(function (response) {
+        console.log(response);
+        axios.get(`https://api.github.com/users/${response.username}`)
+            .then(function (user) {
+                console.log(user.data);
+            })
+    })
+    .then(function (response) {
+        const html = generateHTML(response);
+
+        return writeFileAsync("index.html", html);
+    })
+    .then(function () {
+        console.log("Successfully wrote to index.html");
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+
+function generateHTML() {
+    (function generateHTML(response) {
+        return `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+            <title>Document</title>
+            </head>
+            <body>
+            <div class="jumbotron jumbotron-fluid" styl="background-color: ${response.color}">
+            <div class="container">
+            <img href="${response.user.avatar_url}" style="size: 200px, 200px"/>
+            <br>
+            <h1 class="display-4">Hi! <br> My name is ${response.user.name}</h1>
+            <ul class="list-group">
+            <li class="list-group-item">GitHub: ${response.user.html_url}</li>
+            <li class="list-group-item">Location: ${response.user.location}</li>
+            </ul>
+            <h3>${response.user.bio}</h3>
+            </div>
+            <div class="section">
+            <h3>Public Repositories: ${response.user.public_repos}</h3>
+            <h3>Followers: ${response.user.followers}</h3>
+            <h3>Following: ${response.user.following}</h3>
+            <h3>Starred: ${response.user.starred_url}</h3>
+            
+            </div>
+            </div>
+            </body>
+            </html>`;
+    });
+}
+
+generateHTML();
